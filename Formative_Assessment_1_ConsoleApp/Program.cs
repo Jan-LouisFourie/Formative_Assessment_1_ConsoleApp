51753f88978a5b12e5c73e90f8c97c32cb610a58
Jan-Louis <20231441@ctucareer.co.za>
2026-04-22 14:41:23 +0200

    Add project files.

---
+++ Formative_Assessment_1_ConsoleApp/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace StudentResultApp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            double[] marks = new double[3];
+            double markTotal = 0;
+
+            Console.Write("""
+
+                =====================
+                STUDENT GRADING APP
+                =====================
+                
+                """);
+
+            Console.Write("\nEnter Student Name: ");
+            string studentName = Console.ReadLine();
+
+            for (int i = 0; i < 3; i++)
+            {
+                bool isValid = false;
+                while (!isValid)
+                {
+                    Console.Write($"Enter Mark for Subject {i + 1}: ");
+                    string input = Console.ReadLine();
+
+                    if (double.TryParse(input, out marks[i]))
+                    {
+                        markTotal += marks[i];
+                        isValid = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input! Please enter a numeric value.");
+                    }
+                }
+            }
+
+            double average = markTotal / 3;
+            string result = (average >= 50) ? "PASS" : "FAIL";
+
+            Console.WriteLine("\n");
+            Console.WriteLine($"""
+                =====================
+                       Results
+                =====================
+
+                Student Name    : {studentName}
+                Total Marks     : {markTotal}
+                Average Mark    : {average:f2}%
+                Result          : {result}
+                Result issued at: {DateTime.Now:f}
+                """);
+        }
+    }
+}
