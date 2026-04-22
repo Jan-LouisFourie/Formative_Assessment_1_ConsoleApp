837eacc8a11f537235f875e2f41237a24c4317c2
Jan-Louis <20231441@ctucareer.co.za>
2026-04-22 15:19:14 +0200

    Add initial class stubs for Resident, ServiceRequest, UtilitiesManager
    Created three new internal class files: Resident.cs, ServiceRequest.cs, and UtilitiesManager.cs. Each class is defined within the Formative_Assessment_1_ConsoleApp namespace and currently contains only the class declaration and standard .NET using directives. These stubs will serve as the foundation for future implementation.

---
+++ Formative_Assessment_1_ConsoleApp/ServiceRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formative_Assessment_1_ConsoleApp
+{
+    internal class ServiceRequest
+    {
+    }
+}
